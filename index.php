898f4248528c99d419692d44012e6275f5b19a9d
Andrew <andrew@simplescienceinc.com>
2014-02-27 11:34:50 -0800

    Init

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=1024, initial-scale=1">
+<title>Rabit</title>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+</head>
+<body>
+<header>
+	<div class="container">
+		<h1>AFG Music</h1>
+		<h2>Lessons - Sales - Repairs - Rentals - Performances</h2>
+	</div>
+</header>
+<nav>
+	<div class="container">
+		<ul class="nav">
+			<li><a href="#">Home</a></li>
+			<li>
+				<a href="#">Lessons<i class="fa fa-angle-down"></i></a>
+				<ul>
+					<li><a href="#">Fred Gillett</a></li>
+					<li><a href="#">Anita Gillett</a></li>
+					<li><a href="#">Jon Lundgren</a></li>
+					<li><a href="#">Ron Kobayashi</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Sunset Strings</a></li>
+			<li><a href="#">Preformances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Site Map</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+	</div>
+</nav>
+<div class="hero">
+</div>
+<div class="body">
+	<div class="container">
+
+	</div>
+</div>
+</body>
+</html>
