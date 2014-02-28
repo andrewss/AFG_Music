82816a8edaf55b5532067a85b5b48b529cba4a42
Andrew <atetnowski@gmail.com>
2014-02-27 21:07:18 -0800

    Updated index page

--- index.php
+++ index.php
@@ -53,6 +53,52 @@
 		<p>Our Sunset Strings performs at weddings and other special events.</p>
 		<p>While we are not a high volume sales dealer, we do carry a selection of reasonably priced guitars and a wide assortment of parts, strings, reeds, and accessories for various instruments.  Plus we can order whatever we don't carry in stock.</p>
 		<p>Take some time to explore our site to get information on our lessons and instructors, rental instruments, repairs, and our performances for your special ocassions.</p>
+	
+		<div class="services_container">
+			<h3>Our Services</h3>
+			<div class="single_service lessons">
+				<div class="service_icon_outer">
+					<i class="fa fa-music"></i>
+				</div>
+				<h5>Lessons</h5>
+				<h6>We offer music instruction in the following instruments</h6>
+				<ul>
+					<li>Guitar</li>
+					<li>Piano & Keyboards</li>
+					<li>Violin</li>
+					<li>Cello</li>
+					<li>Banjo</li>
+					<li>Mandolin</li>
+					<li>Drums & Percussion</li>
+					<li>Band Instruments</li>
+				</ul>
+			</div>
+
+			<div class="single_service repair">
+				<div class="service_icon_outer">
+					<i class="fa fa-wrench"></i>
+				</div>
+				<h5>Repair</h5>
+				<h6>We offer repairs on the following types of instruments</h6>
+				<ul>
+					<li>Stringed Instruments</li>
+					<li>Brass Instruments</li>
+					<li>Reed Instruments</li>
+				</ul>
+			</div>
+
+			<div class="single_service rental">
+				<div class="service_icon_outer">
+					<i class="fa fa-exchange"></i>
+				</div>
+				<h5>Rental</h5>
+				<h6>We rent and lease various musical instruments.  We have great programs for band students.</h6>
+			</div>
+		</div>
+		<div class="friends_container">
+			<h3>Our Friends</h3>
+		</div>
+
 	</div>
 </div>
 </body>
