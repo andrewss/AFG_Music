82e7eb8575f5edf57bce316adef0c680ad1aca46
Andrew <atetnowski@gmail.com>
2014-03-02 15:05:57 -0800

    Index Page finished

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 <body>
 <header>
 	<div class="container">
+		<a href="index.php"><img src="img/afg_white.png" alt="Home"></a>
 		<h1>AFG Music</h1>
 		<h3>Lessons - Sales - Repairs - Rentals - Performances</h3>
 	</div>
@@ -46,60 +47,102 @@
 </nav>
 <div class="hero">
 </div>
-<div class="body">
+
+
+<div class="services">
 	<div class="container">
-		<h1>AFG Music</h1>
-		<h2>AFG Music has been in business in Costa Mesa since 1980.  We feature music lessons, sales of musical instruments and supplies, repairs, and instrument rentals. </h2>
-		<p>Our Sunset Strings performs at weddings and other special events.</p>
-		<p>While we are not a high volume sales dealer, we do carry a selection of reasonably priced guitars and a wide assortment of parts, strings, reeds, and accessories for various instruments.  Plus we can order whatever we don't carry in stock.</p>
-		<p>Take some time to explore our site to get information on our lessons and instructors, rental instruments, repairs, and our performances for your special ocassions.</p>
-	
-		<div class="services_container">
-			<h3>Our Services</h3>
-			<div class="single_service lessons">
-				<div class="service_icon_outer">
-					<i class="fa fa-music"></i>
-				</div>
-				<h5>Lessons</h5>
-				<h6>We offer music instruction in the following instruments</h6>
-				<ul>
-					<li>Guitar</li>
-					<li>Piano & Keyboards</li>
-					<li>Violin</li>
-					<li>Cello</li>
-					<li>Banjo</li>
-					<li>Mandolin</li>
-					<li>Drums & Percussion</li>
-					<li>Band Instruments</li>
-				</ul>
+		<h3>Our Services</h3>
+		<div class="single_service lessons">
+			<div class="service_icon_outer">
+				<i class="fa fa-music"></i>
 			</div>
-
-			<div class="single_service repair">
-				<div class="service_icon_outer">
-					<i class="fa fa-wrench"></i>
-				</div>
-				<h5>Repair</h5>
-				<h6>We offer repairs on the following types of instruments</h6>
-				<ul>
-					<li>Stringed Instruments</li>
-					<li>Brass Instruments</li>
-					<li>Reed Instruments</li>
-				</ul>
-			</div>
-
-			<div class="single_service rental">
-				<div class="service_icon_outer">
-					<i class="fa fa-exchange"></i>
-				</div>
-				<h5>Rental</h5>
-				<h6>We rent and lease various musical instruments.  We have great programs for band students.</h6>
-			</div>
-		</div>
-		<div class="friends_container">
-			<h3>Our Friends</h3>
+			<h5>Lessons</h5>
+			<h6>We offer music instruction in the following instruments</h6>
+			<ul class="two_column">
+				<li>Guitar</li>
+				<li>Piano</li>
+				<li>Keyboards</li>
+				<li>Violin</li>
+				<li>Cello</li>
+				<li>Banjo</li>
+				<li>Mandolin</li>
+				<li>Drums</li>
+				<li>Percussion</li>
+				<li>Band Instruments</li>
+			</ul>
+			<a href="#" class="details_btn">Details</a>
 		</div>
 
+		<div class="single_service repair">
+			<div class="service_icon_outer">
+				<i class="fa fa-wrench"></i>
+			</div>
+			<h5>Repair</h5>
+			<h6>We offer repairs on the following types of instruments</h6>
+			<ul>
+				<li>Stringed Instruments</li>
+				<li>Brass Instruments</li>
+				<li>Reed Instruments</li>
+			</ul>
+			<a href="#" class="details_btn">Details</a>
+		</div>
+
+		<div class="single_service rental">
+			<div class="service_icon_outer">
+				<i class="fa fa-exchange"></i>
+			</div>
+			<h5>Rental</h5>
+			<h6>We rent and lease various musical instruments.  We have great programs for band students.</h6>
+			<p>Instrument rental is an ideal way for band students and beginning students to begin learning an instrument without making an expensive investment.</p>
+			<a href="#" class="details_btn">Details</a>
+		</div>
 	</div>
 </div>
+
+<div class="about_us">
+	<div class="container">
+			<h3>About Us</h3>
+			<h2>AFG Music has been in business in Costa Mesa since 1980.  We feature music lessons, sales of musical instruments and supplies, repairs, and instrument rentals. </h2>
+			<p>Our Sunset Strings performs at weddings and other special events.</p>
+			<p>While we are not a high volume sales dealer, we do carry a selection of reasonably priced guitars and a wide assortment of parts, strings, reeds, and accessories for various instruments.  Plus we can order whatever we don't carry in stock.</p>
+			<p>Take some time to explore our site to get information on our lessons and instructors, rental instruments, repairs, and our performances for your special ocassions.</p>
+		</div>
+</div>
+<div class="friends">
+	<div class="container">
+		<h3>Our Friends</h3>
+		<ul>
+			<li>
+				<a href="#">
+					<img src="img/nhhsm.jpg">
+					<span>Newport Harbor High School Music</span>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<img src="img/ds.jpg">
+					<span>Desert Symphony</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<footer>
+	<div class="container">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Lessons</a></li>
+			<li><a href="#">Sunset Strings</a></li>
+			<li><a href="#">Preformances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Site Map</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+		<p>AFG Music &copy; Copyright <?php echo date("Y")?></p>
+	</div>
+</footer>
 </body>
 </html>
