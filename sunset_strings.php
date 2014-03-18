2c5c985015e6027aed2b4f0a3c1946a1c6a0778b
andrewss <andrew@simplescienceinc.com>
2014-03-18 12:12:47 -0700

    Sunset Strings done

---
+++ sunset_strings.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=1024, initial-scale=1">
+<title>AFG Music</title>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+</head>
+<body>
+<header>
+	<div class="container">
+		<a href="index.php"><img src="img/afg_white.png" alt="Home"></a>
+		<h1>AFG Music</h1>
+		<h3>Lessons - Sales - Repairs - Rentals - Performances</h3>
+	</div>
+</header>
+<nav>
+	<div class="container">
+		<ul class="nav">
+			<li><a href="index.php">Home</a></li>
+			<li>
+				<a href="lessons.php">Lessons<i class="fa fa-angle-down"></i></a>
+				<ul>
+					<li><a href="#">Fred Gillett</a></li>
+					<li><a href="#">Anita Gillett</a></li>
+					<li><a href="#">Jon Lundgren</a></li>
+					<li><a href="#">Ron Kobayashi</a></li>
+				</ul>
+			</li>
+			<li><a href="sunset_strings.php">Sunset Strings</a></li>
+			<li><a href="#">Performances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Site Map</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+	</div>
+</nav>
+
+
+<div class="sunset_strings">
+	<div class="container">
+		<h1 class="page_title">Sunset Strings</h1>
+		<img src="img/sunset_strings.jpg">
+		<p class="img_desc">(String Quartet with 2 violins, 1 viola, and 1 cello)</p>
+		<p class="tagline">Sunset Strings is our String Quartet or Strolling Strings group.  We will entertain your guests at your wedding, special events, parties, and corporate events. We serve Orange County and the surrounding areas.</p>
+		<h3>Parties and Corporate Events</h3>
+		<p>The String Quartet is like having a miniature Symphony Orchestra performing in your own venue, whether it be a Hotel, Home, or Boat.  Our Strolling Strings provide a more intimate setting for those special occasions.  We have a professional staff of musicians that come dressed in tuxedos, or less formal attire if you wish.  Our library is very extensive and covers music “From Bach to Rock“.  It includes the traditional Bach, Beethoven, and Mozart.  It also includes ethnic selections such as French, German. Italian, Mexican, etc.  We also have American Patriotic, Broadway Musicals, Beatles, and Movie/TV songs.  Many of our selections are by Irving Berlin, Cole Porter, George Gershwin, Richard Rogers, Scott Joplin, and Henry Mancini.  Our Christmas and Hanukkah selections are perfect for that end of year program.</p>
+		<h3>Weddings</h3>
+		<p>We at Fred’s wish to make your day an exceptional and memorable one.  We have a large selection of wedding processionals and recessionals for you to choose from.  If we don’t have what you want, we will make every effort to acquire it for your listening pleasure.  We will also be happy to make a custom arrangement for inclusion in your ceremony.</p>
+ 
+		<p>Firstly, we design a timeline (outline) of your very unique combination of choices.</p>
+
+		<p>Secondly, you may have a friend or relative that wishes to participate in your very special ceremony.  We try to make their participation comfortable and fun, so they can add their love and blessing to your ceremony.</p>
+
+		<p>Thirdly, we also understand budgeting restraints.  We can cut the viola and go with a String Trio (2 violins and 1 cello) making it more appealing to the budget conscious.  This will not sound as full as the String Quartet, but will certainly have the same elegant ambiance.</p>
+
+		<p>Fourthly, you can have the String Quartet perform for your receiving line and cocktails directly following the ceremony.  This fills the "big gap" between the ceremony and reception.  We can also perform lighter and more modern selections for your listening pleasure.</p>
+
+		<p>Fiftly, you can get two uniquely different groups (for the price of one).  The musicians change from the classical format of the sit down String Quartet to a Strolling Strings group.  The Strolling Strings stroll amongst your guests, performing much lighter love songs, show tunes, and specialty selections.  This style creates a much more intimate feeling, because the music is right there next to your guests.  The Strolling Strings also accommodate any venue, because they are "portable" and free to assume most any configuration.</p>
+	</div>
+</div>
+
+
+<footer>
+	<div class="container">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Lessons</a></li>
+			<li><a href="#">Sunset Strings</a></li>
+			<li><a href="#">Preformances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Site Map</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+		<p>AFG Music &copy; Copyright <?php echo date("Y")?></p>
+	</div>
+</footer>
+</body>
+</html>
