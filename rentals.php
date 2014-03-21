69b848deef7980e2e6a04df30df5ec2b166aae5d
Andrew <atetnowski@gmail.com>
2014-03-20 21:31:54 -0700

    Started on rentals page

---
+++ rentals.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=1024, initial-scale=1">
+<title>AFG Music</title>
+
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,500,700,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+</head>
+<body>
+<header>
+	<div class="container">
+		<a href="index.php"><img src="img/afg_white.png" alt="Home"></a>
+		<h1>AFG Music</h1>
+		<h3>Lessons - Sales - Repairs - Rentals - Performances</h3>
+	</div>
+</header>
+<nav>
+	<div class="container">
+		<ul class="nav">
+			<li><a href="index.php">Home</a></li>
+			<li>
+				<a href="lessons.php">Lessons<i class="fa fa-angle-down"></i></a>
+				<ul>
+					<li><a href="#">Fred Gillett</a></li>
+					<li><a href="#">Anita Gillett</a></li>
+					<li><a href="#">Jon Lundgren</a></li>
+					<li><a href="#">Ron Kobayashi</a></li>
+				</ul>
+			</li>
+			<li><a href="sunset_strings.php">Sunset Strings</a></li>
+			<li><a href="#">Performances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+	</div>
+</nav>
+
+
+<div class="rentals">
+	<div class="container">
+		<h1 class="page_title">Instrument Rentals</h1>
+		<p class="tagline">AFG Music has a rental and lease program.  This is an ideal way for band students and beginning students to begin learning an instrument without making an expensive investment.</p>
+		<h3>Rental Program (1st Year Program)</h3>
+		<p>The rental program provides <em>Used Instruments</em> (New when available) and is designed for those people wishing to rent instruments without long-term commitment.  Payments range from $21.00 to $60.00 per month depending on the instrument desired,  This fee also includes theft and damage coverage.  Six months rental credit may apply to the price of that instrument or any other instrument of equal of greater value (consignment instruments excluded).</p>
+		<h3>Lease Program (2nd Year Program)</h3>
+		<p>The lease program provides <em>New instruments</em> and is designed for those people wishing to purchase a better instrument with a convenient Zero Interest payment plan.  The payments range from $30.00/up per month depending upon the instrument desired.  <em>100% of payments apply to the price of the instrument.</em>  The term may vary from 20 months/up.<br><br><em>FORMULA = Cost  Tax / Monthly payment = Months</em></p>
+		<h3>Signup Requirements</h3>
+		<ul>
+			<li>Valid California Drivers License (Identification)</li>
+			<li>Current MasterCard or Visa (Deposit)</li>
+			<li>Fully completed and signed application.</li>
+			<li>Three Months Minimum.</li>
+		</ul>
+ 		<table>
+ 			<thead>
+ 				<tr>
+ 					<th>Instrument</th>
+ 					<th>Rental (Used)</th>
+ 					<th>Rental (New)</th>
+ 					<th>Lease (New)</th>
+ 				</tr>
+ 			</thead>
+ 			<tbody>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Flute</td>
+ 					<td>$21</td>
+ 					<td>$32</td>
+ 					<td>$37 @ 22 Months</td>
+ 				</tr>
+ 			</tbody>
+ 		</table>
+	</div>
+</div>
+
+
+<footer>
+	<div class="container">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Lessons</a></li>
+			<li><a href="#">Sunset Strings</a></li>
+			<li><a href="#">Preformances</a></li>
+			<li><a href="#">Rentals</a></li>
+			<li><a href="#">Repairs</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">About Us</a></li>
+		</ul>
+		<p>AFG Music &copy; Copyright <?php echo date("Y")?></p>
+	</div>
+</footer>
+</body>
+</html>
