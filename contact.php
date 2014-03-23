3aba80bbb66510c0ba5d1707f16ca297d62d773e
Andrew <atetnowski@gmail.com>
2014-03-23 16:11:00 -0700

    Reapirs

--- contact.php
+++ contact.php
@@ -49,7 +49,39 @@
 <div class="contact">
 	<div class="container">
 		<h1 class="page_title">Contact</h1>
-		<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=711+West+17th+St.+Suite+F1+Costa+Mesa,+Ca.+92627&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=57.42297,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=711+W+17th+St,+Costa+Mesa,+California+92627&amp;t=m&amp;z=14&amp;ll=33.634883,-117.927863&amp;output=embed"></iframe>
+		<div class="loc_container">
+			<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=711+West+17th+St.+Suite+F1+Costa+Mesa,+Ca.+92627&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=57.42297,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=711+W+17th+St,+Costa+Mesa,+California+92627&amp;t=m&amp;z=14&amp;ll=33.634883,-117.927863&amp;output=embed"></iframe>
+			<div class="address_container">
+				<p class="address">AFG Music</p>
+				<p class="address">711 West 17th St. Suite F1</p>
+				<p class="address">Costa Mesa, Ca. 92627</p>
+				<p class="address">(949) 642-1686</p>
+				<p class="address">(949) 548-7898</p>
+				<p class="address_note">We are located a few blocks from 17th. St and Harbor Blvd. in Costa Mesa. Our address is on 17th street, but we are located in the back of the complex. To get to us, take Pomona St. to Farad, which is one block south of 17th street, and turn in the driveway on the right.</p>
+			</div>
+			<form method="POST">
+				<div class="input_container">
+					<label>First Name<span class="req">*<span></label>
+					<input type="text" name="fname">
+				</div>
+				<div class="input_container">
+					<label>Last Name<span class="req">*<span></label>
+					<input type="text" name="lname">
+				</div>
+				<div class="input_container">
+					<label>Email<span class="req">*<span></label>
+					<input type="text" name="email">
+				</div>
+				<div class="input_container">
+					<label>Phone Number</label>
+					<input type="text" name="phone">
+				</div>
+				<div class="textarea_container">
+					<label>Message<span class="req">*<span></label>
+					<textarea name="message"></textarea>
+				</div>
+			</form>
+		</div>
 	</div>
 </div>
 
